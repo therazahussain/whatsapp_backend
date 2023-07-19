8bfa9c323c2d768396badbc1b4f52baf14eda477
Raza Husain <razahussain3301@gmail.com>
2023-07-19 18:40:17 +0530

    new

--- routes/webhook.js
+++ routes/webhook.js
@@ -29,12 +29,12 @@ router.post("/webhook", (req, res) => {
     if (
       body_param.entry &&
       body_param.entry[0].changes &&
-      body_param.entry[0].changes[0].value.message &&
-      body_param.entry[0].changes[0].value.message[0]
+      body_param.entry[0].changes[0].value.messages &&
+      body_param.entry[0].changes[0].value.messages[0]
     ) {
       let phone_no_id =
         body_param.entry[0].changes[0].value.metadata.phone_number_id;
-      let from = body_param.entry[0].changes[0].value.message[0].from;
+      let from = body_param.entry[0].changes[0].value.messages[0].from;
       let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
       console.log(msg_body);
